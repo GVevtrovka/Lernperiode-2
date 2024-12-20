9648695c120958ed536215c578b00a2d747cc26b
General_Vevtrovka <mathieu.devries@bluewin.ch>
2024-12-20 11:09:15 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,257 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks.Dataflow;
+namespace Donkeykong
+{
+    public class Programm
+    {
+        public static int Timer;
+        public static bool intime;
+        public static int repeats;
+        public static char Jumpmovement;
+        public static bool Walklock = false;
+        public static bool Anschalten = false;
+        public static int xposMario2;
+        public static int yposMario2;
+        public static char Steuerung;
+        public static int xposMario;
+        public static int yposMario;
+        public static int xposBall;
+        public static int yposBall;
+        public static bool GameRunning = true;
+        public static int xposBall2;
+        public static int yposBall2;
+        public static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Char Startkey;
+            Console.WriteLine("Gehen sie ins Vollbild und starten sie das Spiel");
+            while (Anschalten == false)
+            {
+                Startkey = Console.ReadKey().KeyChar;
+                switch (Startkey)
+                {
+                    case 's':
+                        Anschalten = true;
+                        break;
+                    case 'S':
+                        Anschalten = true;
+                        break;
+                    default:
+                        Anschalten = false;
+                        Console.WriteLine("Drücken sie S/sss zu, starten.");
+                        break;
+                }
+                {
+                    xposMario = 0;
+                    yposMario = 29;
+                    xposBall = 0;
+                    yposBall = 2;
+                }
+                Console.Clear();
+            }
+            Thread a = new Thread(MarioControll);
+            Thread d = new Thread(steuerung);
+            a.Start();
+            d.Start();
+        }
+        public static void MarioControll()
+        {
+            while (GameRunning == true)
+            {
+                Console.SetCursorPosition(0, 0);
+                Structure();
+                Mario();
+                Ball();
+                Thread.Sleep(30);
+            }
+        }
+
+        public static void steuerung()
+        {
+            while (GameRunning == true)
+            {
+
+                Steuerung = Console.ReadKey(true).KeyChar;
+                switch (Steuerung)
+                {
+                    case ' ':
+                        if (xposMario <= 19 && xposMario >= 25 && yposMario <= 24 && yposMario >= 12)
+                        {
+                        }
+                        else
+                        {
+                            yposMario -= 3;
+
+                            repeats = 0;
+                            here:
+                            repeats++;
+                            Thread.Sleep(50);
+                            if (Console.KeyAvailable == true && repeats < 20)
+                            {
+                                Jumpmovement = Console.ReadKey(true).KeyChar;
+                                switch (Jumpmovement)
+                                {
+                                    case 'd':
+                                        if (xposMario >= 0 && xposMario <= 150)
+                                        {
+                                            xposMario++;
+                                            goto here;
+                                        }
+                                        else
+                                        {
+                                            goto here;
+                                        }
+                                        break;
+
+                                    case 'a':
+                                        if (xposMario >= 1 && xposMario <= 151)
+                                        {
+                                            xposMario--;
+                                            goto here;
+                                        }
+                                        else
+                                        {
+                                            goto here;
+                                        }
+                                        break;
+                                }
+                            }
+                            else
+                            {
+                                if(repeats < 20)
+                                {
+                                    goto here;
+                                }
+                                else
+                                {
+                                    yposMario += 3;
+                                }
+                            }
+                        }
+                        break;
+                    case 'a':
+                        if (xposMario >= 1 && xposMario <= 151)
+                        {
+                            xposMario--;
+                        }
+                        break;
+                    case 's':
+
+                        break;
+                    case 'd':
+                        if (xposMario >= 0 && xposMario <= 150)
+                        {
+                            xposMario++;
+                        }
+                        break;
+                }
+            }
+        }
+        public static void Mario()
+        {
+            xposBall2 = xposBall;
+            yposBall2 = yposBall;
+            xposMario2 = xposMario;
+            yposMario2 = yposMario;
+            Console.SetCursorPosition(xposMario2, yposMario2);
+            yposMario2++;
+            Console.SetCursorPosition(xposMario2, yposMario2);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write(" ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write(" ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write(" ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write(" ");
+            yposMario2++;
+            Console.SetCursorPosition(xposMario2, yposMario2);
+            Console.Write(" ");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write(" ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write(" ");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write(" ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write(" ");
+            yposMario2++;
+            Console.SetCursorPosition(xposMario2, yposMario2);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write(" ");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write("   ");
+            yposMario2++;
+            Console.SetCursorPosition(xposMario2, yposMario2);
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+        public static void Ball()
+        {
+            Console.SetCursorPosition(xposBall2, yposBall2);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Write("  ");
+            yposBall2++;
+            Console.SetCursorPosition(xposBall2, yposBall2);
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Write("  ");
+            yposBall2++;
+            Console.SetCursorPosition(xposBall2, yposBall2);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Write("  ");
+            yposBall2++;
+            Console.SetCursorPosition(xposBall2, yposBall2);
+            Console.ResetColor();
+        }
+        public static void Structure()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("                                                                                                                                                            ");
+            Console.WriteLine("                                                                                                                                                            ");
+            Console.WriteLine("                                                                                                                                                            ");
+            Console.WriteLine("                                                                                                                                                            ");
+            Console.WriteLine("▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁");
+            Console.WriteLine("╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳");
+            Console.WriteLine("▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔");
+            Console.WriteLine("                                                                                                                                                            ");
+            Console.WriteLine("                                                                                                                                                            ");
+            Console.WriteLine("                                                                                                                                                            ");
+            Console.WriteLine("                                                                                                                                                            ");
+            Console.WriteLine("▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁");
+            Console.WriteLine("╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳");
+            Console.WriteLine("▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔");
+            Console.WriteLine("                                                                                                                                                            ");
+            Console.WriteLine("                                                                                                                                                            ");
+            Console.WriteLine("                                                                                                                                                            ");
+            Console.WriteLine("                                                                                                                                                            ");
+            Console.WriteLine("▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁");
+            Console.WriteLine("╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳");
+            Console.WriteLine("▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔┣━━━┫▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔");
+            Console.WriteLine("                                               ┣━━━┫                                                                                                        ");
+            Console.WriteLine("                                               ┣━━━┫                                                                                                        ");
+            Console.WriteLine("                                               ┣━━━┫                                                                                                        ");
+            Console.WriteLine("                                               ┣━━━┫                                                                                                        ");
+            Console.WriteLine("▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁┣━━━┫▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁");
+            Console.WriteLine("╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳");
+            Console.WriteLine("▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔");
+            Console.WriteLine("                                                                                                                                                            ");
+            Console.WriteLine("                                                                                                                                                            ");
+            Console.WriteLine("                                                                                                                                                            ");
+            Console.WriteLine("                                                                                                                                                            ");
+            Console.WriteLine("▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁");
+            Console.WriteLine("╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳");
+            Console.WriteLine("▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔");
+        }
+    }
+}
